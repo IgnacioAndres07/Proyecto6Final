c3aa7d03a7c5d96f5f35d1d2c7c92c2397bcd698
Ignacio Andrés Gómez Díaz <ignacio.gomez286@gmail.com>
2024-01-27 19:25:43 -0300

    Me encuentro añadiendo numero de whatsapp llamado a la acción final

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -41,7 +41,7 @@ const HeroSection = () => {
           </p>
           <div>
             <Link
-              href="/#contact"
+              href="https://wa.me/56999437664"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
             >
               Hablemos
